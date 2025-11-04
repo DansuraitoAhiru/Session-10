3417a201f1c4560265c85b34cf4f0e19a25e15b7
DansuraitoAhiru <vqviet2007@gmail.com>
2025-11-04 22:41:15 +0700

    Update Vũ Quang Việt_B25DTCN217_IT102_Session10_BTVN07.cpp

--- SS10/Vũ Quang Việt_B25DTCN217_IT102_Session10_BTVN07.cpp
+++ SS10/Vũ Quang Việt_B25DTCN217_IT102_Session10_BTVN07.cpp
@@ -1,24 +1,32 @@
 #include <stdio.h>
-int main(){
-	int number[]={1,4,5,36,37,39,49,199,399,4995};
-	int searchValue;
-	printf("Nhap so can tim: ");
-	scanf("%d", &searchValue);
-	int left,right,mid;
-	left=0;
-	right=sizeof(number)/sizeof(int);
-	do {
-		mid=left + (right-left)/2;
-		if(searchValue<number[mid]){
-			right=mid-1;
-		}
-		if(searchValue>number[mid]){
-			left=mid+1;
-		}
-	} while (searchValue != number[mid] && left != right);
-	if(number[mid]==searchValue){
-		printf("\nPhan tu chi so %d co gia tri bang %d",mid,searchValue);
-	} else {
-		printf("\nKo tim thay phan tu co gia tri bang %d", searchValue);
-	}
+#include <stdio.h>
+int main() {
+    int number[] = {1, 4, 5, 36, 37, 39, 49, 199, 399, 4995};
+    int searchValue;
+    printf("Nhap so can tim: ");
+    scanf("%d", &searchValue);
+
+    int left = 0;
+    int right = sizeof(number) / sizeof(int) - 1;
+    int mid;
+    int found = 0;
+
+    while(left <= right) {
+        mid = left + (right - left) / 2;
+        if(number[mid] == searchValue) {
+            found = 1;
+            break;
+        } else if(searchValue < number[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    if(found) {
+        printf("\nPhan tu chi so %d co gia tri %d", mid, searchValue);
+    } else {
+        printf("\nKhong tim thay gia tri %d trong mang!", searchValue);
+    }
+    return 0;
 }
